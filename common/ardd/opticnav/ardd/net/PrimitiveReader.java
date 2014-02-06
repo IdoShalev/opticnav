ce1fa8d3f89823533fdcdb02e09fda93969e0ce6
Dan Spencer <daniel.spencer@edu.sait.ca>
2014-02-06 06:06:39 +0000

    ard-daemon: PrimitiveReader and PrimitiveWriter, for networking
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@22 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

---
+++ common/ardd/opticnav/ardd/net/PrimitiveReader.java
@@ -0,0 +1,59 @@
+package opticnav.ardd.net;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * PrimitiveReader can read any data type from the ardd protocols.
+ * 
+ * All integers are little-endian.
+ *
+ */
+public final class PrimitiveReader {
+    private InputStream in;
+    
+    public PrimitiveReader(InputStream input) {
+        this.in = input;
+    }
+    
+    public byte[] readBlob() throws IOException {
+        int length = readSInt32();
+        assert length > 0;
+        
+        byte[] buf = new byte[length];
+        this.in.read(buf);
+        return buf;
+    }
+    
+    public String readString() throws IOException {
+        int length = readUInt16();
+        assert length > 0;
+        
+        byte[] buf = new byte[length];
+        this.in.read(buf);
+        return new String(buf, "UTF-8");
+    }
+    
+    public int readUInt16() throws IOException {
+        return (int)readUInt(2);
+    }
+    
+    public int readSInt32() throws IOException {
+        return (int)readUInt(4);
+    }
+    
+    private long readUInt(int bytes) throws IOException {
+        assert bytes < 8;
+        
+        byte[] buf = new byte[bytes];
+        this.in.read(buf);
+        
+        int value = buf[0];
+        
+        for (int i = 1; i < bytes; i++) {
+            value |= buf[i] >> (i*8);
+        }
+        
+        return value;
+    }
+}
---
+++ common/ardd/opticnav/ardd/net/PrimitiveWriter.java
@@ -0,0 +1,58 @@
+package opticnav.ardd.net;
+
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * PrimitiveWriter can write any data type from the ardd protocols.
+ * 
+ * All integers are little-endian.
+ *
+ */
+public class PrimitiveWriter implements Flushable {
+    private OutputStream out;
+    
+    public PrimitiveWriter(OutputStream output) {
+        this.out = output;
+    }
+    
+    public void writeBlob(byte[] buf) throws IOException {
+        writeSInt32(buf.length);
+        this.out.write(buf);
+    }
+    
+    public void writeString(String s) throws IOException {
+        byte[] buf = s.getBytes("UTF-8");
+        writeUInt16(buf.length);
+        this.out.write(buf);
+    }
+    
+    public void writeUInt16(int value) throws IOException {
+        writeUInt(value, 2);
+    }
+
+    public void writeSInt32(int value) throws IOException {
+        writeUInt(value, 4);
+    }
+
+    private void writeUInt(long value, int bytes) throws IOException {
+        assert bytes < 8;
+        
+        byte[] buf = new byte[bytes];
+        
+        long temp_val = value;
+        
+        for (int i = 0; i < bytes; i++) {
+            buf[i] = (byte)(temp_val & 0xFF);
+            temp_val >>= 8;
+        }
+
+        this.out.write(buf);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        this.out.flush();
+    }
+}
