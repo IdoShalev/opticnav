063c1c65bcaf7ff4362b75f3f3a3be92cb0814a9
Dan Spencer <daniel.spencer@edu.sait.ca>
2014-02-09 21:51:28 +0000

    team: More software (wireshark, aptitude)
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@35 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- team/install-software.sh
+++ team/install-software.sh
@@ -3,4 +3,9 @@
 # This script should be run periodically in order to ensure we're all running
 # the same software.
 
-sudo apt-get install mysql-workbench bless
+sudo apt-get install \
+mysql-workbench \
+bless \
+wireshark \
+aptitude \
+
