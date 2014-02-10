b8f4f7273a1562a25dc34634de77c1f773a5f510
Jacky Huynh <jacky.huynh@edu.sait.ca>
2014-02-10 21:37:23 +0000

    Web: Created .ard tag, moved JS to one JS.
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@37 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- web/WebApp/WebContent/ARDRegister.jsp
+++ web/WebApp/WebContent/ARDRegister.jsp
@@ -1,7 +1,7 @@
-<t:page title="Register Device" js="ARDRegisterTest.js">
+<t:page title="Register Device" js="registerARD.js">
     <h1>Register Device</h1>
     Current Registered Device:<br/>
-    <p style="border-style:solid; border-color:grey; background-color:light-grey;">None</p>
+    <p id="ard">None</p>
     <form id="RegisterARD" action="op/RegisterARD">
     Confirmation Code:<br/> <input type="text" name="code" id="code" />
     <input type="submit" value="Register" id="register" />
--- web/WebApp/WebContent/js/registerARD.js
+++ web/WebApp/WebContent/js/registerARD.js
@@ -1,17 +1,26 @@
 $(function() {
-	$("#register").submit(function(event) {
+	$("#RegisterARD").submit(function(event) {
 		var code = $("#code").val();
 		
-		var credentials = {code: wXXyZ};
+		var credentials = {code: code};
 		
-		$.post("register", credentials, function(data) {
-			if (data.successful) {
-				window.location.href = "auth";
-			} else {
-				showErrorMessage("message", data.message);
+		$.post("op/RegisterARD", credentials,function(data) {
+			if (data.successful){
+				showSuccessMessage("message", data.message);
+				updateARDS();
 			}
+			showErrorMessage("message", data.message);
 		}, "json");
 		
 		event.preventDefault();
 	});
+	
+	function updateARDs() {
+        $.post("op/ARDInfo",function(data) {
+        	var id = data.id;
+        	var name = data.name;
+        	$('#ard').html(id+" "+name);
+        }, "json");
+	}
+	$("#ARDInfo").ready(updateARDs);
 });
