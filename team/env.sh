907671e9cc849df1320157437857fbca9a5a87a0
Dan Spencer <daniel.spencer@edu.sait.ca>
2014-02-12 05:52:34 +0000

    team: env.sh: Example command to append env.sh to ~/.bashrc
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@41 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- team/env.sh
+++ team/env.sh
@@ -1,5 +1,6 @@
 # set the team environment
 # in ~/.bashrc, add "source /cs/trunk/team/env.sh" at the end (without quotes)
+# echo "source /cs/trunk/team/env.sh" >> ~/.bashrc
 
 DIR=$(dirname $BASH_SOURCE)
 
