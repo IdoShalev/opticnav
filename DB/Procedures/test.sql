453f756a562af9f689656426f58697c9f4659682
Kay Bernhardt <kay.bernhardt@edu.sait.ca>
2014-02-16 19:18:04 +0000

    DB: changed some tests
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@51 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- DB/Procedures/test.sql
+++ DB/Procedures/test.sql
@@ -1,24 +1,31 @@
-DELETE FROM ACCOUNT;
 source /cs/trunk/DB/tables.sql;
-
 
 CALL registerAccount('kay', 'password');
 CALL registerAccount('ido', 'password');
 CALL registerAccount('danny', 'password');
+CALL registerAccount('kay', 'password');
 
 SELECT * FROM WEB_ACCOUNT;
 
 SELECT validateUser('kay', 'password');
-SELECT validateUser('kay', 'pass');
+SELECT validateUser('ido', 'password');
+SELECT validateUser('danny', 'password');
+SELECT validateUser('kay', 'fail');
 
 SELECT checkAccountName('kay');
+SELECT checkAccountName('ido');
+SELECT checkAccountName('danny');
 SELECT checkAccountName('fail');
 
 SELECT findAccount('kay');
+SELECT findAccount('ido');
+SELECT findAccount('danny');
 SELECT findAccount('fail');
 
 CALL createARD('tacocat', 'password');
 CALL createARD('theard', 'pass');
+CALL createARD('theard', 'lol');
+CALL createARD('fail?', 'password');
 
 SELECT * FROM ARD;
 
@@ -28,7 +35,13 @@ SELECT validateARD('fail');
 
 CALL connectARD(1, 1);
 CALL connectARD(2, 2);
+CALL connectARD(1, 2);
 
+SELECT getARD(1);
 SELECT getARD(2);
+SELECT getARD(3);
+SELECT getARD(4);
 
 SELECT getARDNameByARDID(1);
+SELECT getARDNameByARDID(2);
+SELECT getARDNameByARDID(3);
