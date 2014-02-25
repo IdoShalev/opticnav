dea37a81dda37aff9a4ddd2ff955e0c63bc5f9bf
Kay Bernhardt <kay.bernhardt@edu.sait.ca>
2014-02-25 22:30:54 +0000

    DB Broker: added first 4 methods (untested)
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@72 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- persistence/web-db-broker/src/main/java/opticnav/persistence/web/WebDBBroker.java
+++ persistence/web-db-broker/src/main/java/opticnav/persistence/web/WebDBBroker.java
@@ -6,24 +6,60 @@ import java.sql.SQLException;
 import com.mysql.jdbc.Connection;
 
 public class WebDBBroker {
-    private Connection conn;
+    private java.sql.Connection conn;
 
-    public WebDBBroker(Connection conn) {
+    public WebDBBroker(java.sql.Connection conn) {
         this.conn = conn;
     }
     
     public boolean registerAccount(String username, String password) throws WebDBBrokerException {
-        try (CallableStatement cs = conn.prepareCall("call whatever")) {
-            cs.executeUpdate();
+        try (CallableStatement cs = conn.prepareCall("{call registerAccount(?, ?}")) {
+            cs.setString(1, username);
+            cs.setString(2, password);
+            
+            cs.execute();
             
             // account registered!
             return true;
         } catch (SQLException e) {
             throw new WebDBBrokerException(e);
         }
+    }    
+    
+    public int verify(String username, String password) throws WebDBBrokerException {
+        try (CallableStatement cs = conn.prepareCall("{? = call validateUser(?, ?}")) {
+            cs.setString(2, username);
+            cs.setString(3, password);
+            
+            cs.execute();
+            
+            return cs.getInt(1);
+        } catch (SQLException e) {
+            throw new WebDBBrokerException(e);
+        }
     }
     
-    public boolean verify(String username, String password) throws WebDBBrokerException {
-        return false;
+    public int findName(String username) throws WebDBBrokerException {
+        try (CallableStatement cs = conn.prepareCall("{? = call findAccount(?}")) {
+            cs.setString(2, username);
+            
+            cs.execute();
+            
+            return cs.getInt(1);
+        } catch (SQLException e) {
+            throw new WebDBBrokerException(e);
+        }
+    }
+    
+    public Boolean checkName(String username) throws WebDBBrokerException {
+        try (CallableStatement cs = conn.prepareCall("{? = call checkAccountName(?}")) {
+            cs.setString(2, username);
+            
+            cs.execute();
+            
+            return cs.getBoolean(1);
+        } catch (SQLException e) {
+            throw new WebDBBrokerException(e);
+        }
     }
 }
--- persistence/web-db-broker/src/test/java/AccountTest.java
+++ persistence/web-db-broker/src/test/java/AccountTest.java
@@ -1,8 +1,43 @@
 import static org.junit.Assert.*;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import opticnav.persistence.web.WebDBBroker;
+import opticnav.persistence.web.WebDBBrokerException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AccountTest {
+    private java.sql.Connection conn;
+    private WebDBBroker broker;
+    
+    @BeforeClass
+    public void startUp() throws ClassNotFoundException, SQLException {
+        // mysql connection stuff lol
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        this.conn = DriverManager.getConnection("jdbc:mysql://localhost/OpticNavDB?"
+                    + "user=test&password=password");
+        
+        broker = new WebDBBroker(conn);
+    }
+    
+    @AfterClass
+    public void tearDown() throws SQLException {
+        conn.close();
+    }
+    
     @Test
-    public void test() {
+    public void testregisterAccount() throws WebDBBrokerException {
+        assertEquals(true, broker.registerAccount("kay", "password"));
+    }
+    
+    @Test
+    public void testverify() throws WebDBBrokerException {
+        assertEquals(1, broker.verify("kay", "password"));
     }
 }
