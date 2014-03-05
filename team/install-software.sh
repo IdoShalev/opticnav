caf286e2360d3b06b316729881044e8aef2cc7b2
Dan Spencer <daniel.spencer@edu.sait.ca>
2014-03-05 06:18:03 +0000

    TEAM: Downgrade Gradle to 1.10 in order to work with the Android Gradle plugin
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@111 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- team/install-software.sh
+++ team/install-software.sh
@@ -10,6 +10,7 @@ wireshark \
 aptitude \
 
 # Gradle is needed for building the project
-# If gradle-1.11 is not available (returns error), install the PPA repo
-sudo apt-get install gradle-1.11 || (sudo add-apt-repository ppa:cwchien/gradle && sudo apt-get update)
-
+# If gradle-1.10 is not available (returns error), install the PPA repo
+sudo apt-get install gradle-1.10 || (sudo add-apt-repository ppa:cwchien/gradle && sudo apt-get update && sudo apt-get install gradle-1.10)
+# Gradle 1.11 should be removed, as it doesn't work with the Android gradle tools
+sudo apt-get remove gradle-1.11
