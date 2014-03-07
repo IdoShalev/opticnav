6e5a54b53f621820f676b79b9d6a6db1564a7a0a
Kay Bernhardt <kay.bernhardt@edu.sait.ca>
2014-03-07 16:39:12 +0000

    DB: edited tables Added: resource_data to RESOURCE web_account_id to MAP (FOREIGN KEY to WEB_ACCOUNT) resource_name to RESOURCE Removed: WEB_ACCOUNT_OWNED_MAPS table
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@119 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- DB/tables.sql
+++ DB/tables.sql
@@ -9,31 +9,26 @@ CREATE TABLE WEB_ACCOUNT
 CREATE TABLE RESOURCE
 (
     resource_id         INT(4)      AUTO_INCREMENT PRIMARY KEY,
-    resource_type       VARCHAR(16) NOT NULL
+    resource_type       VARCHAR(16) NOT NULL,
+    resource_data       MEDIUMBLOB  NOT NULL
 );
 
 CREATE TABLE MAP
 (
     map_id              INT(4)      AUTO_INCREMENT PRIMARY KEY,
+    web_account_id      INT(4)      NOT NULL,
     name                VARCHAR(64) NOT NULL,
     resource_id         INT(4)      NOT NULL,
-    CONSTRAINT          fk_map_resource FOREIGN KEY (resource_id) REFERENCES RESOURCE(resource_id)
-);
-
-CREATE TABLE WEB_ACCOUNT_OWNED_MAPS
-(
-    web_account_id      INT(4),
-    map_id              INT(4),
-    PRIMARY KEY(web_account_id, map_id),
-    CONSTRAINT          fk_web_account_owned_maps_web_account FOREIGN KEY (web_account_id) REFERENCES WEB_ACCOUNT(web_account_id),
-    CONSTRAINT          fk_web_account_owned_maps_map FOREIGN KEY (map_id) REFERENCES MAP(map_id)
+    CONSTRAINT          fk_map_resource FOREIGN KEY (resource_id) REFERENCES RESOURCE(resource_id),
+    CONSTRAINT          fk_map_web_account FOREIGN KEY (web_account_id) REFERENCES WEB_ACCOUNT(web_account_id)
 );
 
 CREATE TABLE MARKER
 (
     marker_id           INT(4)      AUTO_INCREMENT PRIMARY KEY,
+    marker_name         VARCHAR(64) NOT NULL,
     map_id              INT(4)      NOT NULL,
-    resource_id         INT(4)      NOT NULL,
+    resource_id         INT(4),
     lat                 INT(4)      NOT NULL,
     longitude           INT(4)      NOT NULL,
     CONSTRAINT          fk_marker_map FOREIGN KEY (map_id) REFERENCES MAP(map_id),
