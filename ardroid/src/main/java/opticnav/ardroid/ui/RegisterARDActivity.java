9897c030668c5f4c03fdb50fcd8ac7717d76237b
Dan Spencer <daniel.spencer@edu.sait.ca>
2014-03-13 07:10:03 +0000

    ARDroid: Register ARD Activity
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@139 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

---
+++ ardroid/src/main/java/opticnav/ardroid/ui/RegisterARDActivity.java
@@ -0,0 +1,13 @@
+package opticnav.ardroid.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import opticnav.ardroid.R;
+
+public class RegisterARDActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerard);
+    }
+}
--- ardroid/src/main/java/opticnav/ardroid/ui/WelcomeActivity.java
+++ ardroid/src/main/java/opticnav/ardroid/ui/WelcomeActivity.java
@@ -1,10 +1,13 @@
 package opticnav.ardroid.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.PictureDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGParseException;
@@ -16,9 +19,6 @@ public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // don't include a title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_welcome);
 
         ImageView opticNavLogo = (ImageView)findViewById(R.id.opticNavLogo);
@@ -29,6 +29,15 @@ public class WelcomeActivity extends Activity {
         } catch (SVGParseException e) {
         } catch (IOException e) {
         }
+
+        Button detectServer = (Button)findViewById(R.id.detectServer);
+        detectServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, RegisterARDActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
