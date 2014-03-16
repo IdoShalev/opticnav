a8dde46bde7b891c219508db007dffbb46c1476a
Kay Bernhardt <kay.bernhardt@edu.sait.ca>
2014-03-16 00:24:11 +0000

    DB: added some methods to the AccountBroker (DO NOT USE BROKEN) Added: createMap() deleteMap() modifyMap() getMapResource()
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@143 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- persistence/web-db-broker/src/main/java/opticnav/persistence/web/AccountBroker.java
+++ persistence/web-db-broker/src/main/java/opticnav/persistence/web/AccountBroker.java
@@ -3,6 +3,7 @@ package opticnav.persistence.web;
 import java.sql.CallableStatement;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.LinkedList;
 
 public class AccountBroker {
     private java.sql.Connection conn;
@@ -45,6 +46,103 @@ public class AccountBroker {
             
             cs.execute();
             conn.commit();
+        } catch (SQLException e) {
+            throw new AccountBrokerException(e);
+        }
+    }
+    
+    //TODO please check=========================================================
+    
+    public int createMap(String name, int imageResource) throws AccountBrokerException{
+        try (CallableStatement cs = conn.prepareCall("{? = call createMap(?, ?, ?)}")){
+            cs.registerOutParameter(1, Types.INTEGER);
+            cs.setString(2, name);
+            cs.setInt(3, imageResource);
+            cs.setInt(4, this.accountID);
+            
+            cs.execute();
+            int id = cs.getInt(1);
+            cs.close();
+            conn.commit();
+            
+            return id;
+        } catch (SQLException e) {
+            throw new AccountBrokerException(e);
+        }
+    }
+    
+    public void modifyMap(int id, Map map) throws AccountBrokerException {
+        try(CallableStatement cs = conn.prepareCall("{call deleteMarker(?)}")){
+            cs.setInt(1, id);
+            cs.execute();
+            
+            //cs = conn.prepareCall("{call deleteAnchor(?)}");
+            cs.setInt(1, id);
+            cs.execute();
+            
+            /*
+            LinkedList<Marker> ml = map.getMarkerList();            
+            for (int i = 0; i < ml.length(); i++){
+                cs = conn.prepareCall("{? = call createMarker(?, ?, ?, ?, ?)}");
+                cs.registerOutParameter(1, Types.INTEGER);
+                cs.setString(2, ml.get(i).getName());
+                cs.setInt(3, id);
+                cs.setInt(4, 0);
+                cs.setInt(5, ml.get(i).getLat);
+                cs.setInt(6, ml.get(i).getLong);
+                
+                cs.execute();
+            }
+            */
+            /*
+            LinkedList<Anchor> al = map.getAnchorList();
+            for (int i = 0; i < al.length(); i++){
+                cs = conn.prepareCall("{call createAnchor(?, ?, ?, ?, ?)}");
+                cs.setInt(1, id);
+                cs.setInt(2, al.get(i).getLocalX);
+                cs.setInt(3, al.get(i).getLocalY);
+                cs.setInt(4, al.get(i).getLat);
+                cs.setInt(5, al.get(i).getLong);
+                
+                cs.execute();
+            }
+            */
+            cs.close();
+            conn.commit();
+            
+        } catch (SQLException e) {
+            throw new AccountBrokerException(e);
+        }
+    }
+    
+    public void deleteMap(int id) throws AccountBrokerException {
+        try(CallableStatement cs = conn.prepareCall("{call deleteMap(?)}")){
+            cs.setInt(1, id);
+            cs.execute();
+            cs.close();
+            conn.commit();
+        } catch (SQLException e) {
+            throw new AccountBrokerException(e);
+        }
+            
+    }
+    
+    //TODO change procedures
+    /*
+    public Map getMap(int id) throws AccountBrokerException {
+        
+    }
+    */
+    
+    public int getMapResource(int id) throws AccountBrokerException {
+        try(CallableStatement cs = conn.prepareCall("{? = call getMapResource(?)}")){
+            cs.registerOutParameter(1, Types.INTEGER);
+            cs.setInt(2, id);
+            
+            cs.execute();
+            id = cs.getInt(1);
+            cs.close();
+            return id;
         } catch (SQLException e) {
             throw new AccountBrokerException(e);
         }
