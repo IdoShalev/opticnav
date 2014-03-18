c695368f84bb049478734e06f8d7bde572eb0df2
Kay Bernhardt <kay.bernhardt@edu.sait.ca>
2014-03-18 21:35:02 +0000

    DB: deleteMap calls deleteAnchor and deleteMarker now
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@162 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- DB/Procedures/deleteMap.sql
+++ DB/Procedures/deleteMap.sql
@@ -7,9 +7,12 @@ DROP PROCEDURE IF EXISTS deleteMap;
 
 DELIMITER //
 
-CREATE PROCEDURE DeleteMap 
+CREATE PROCEDURE deleteMap 
 (p_map_id INT(4))
 BEGIN
+    CALL deleteAnchor(p_map_id);
+    CALL deleteMarker(p_map_id);
+
     DELETE FROM MAP
     WHERE map_id = p_map_id;
 END//
