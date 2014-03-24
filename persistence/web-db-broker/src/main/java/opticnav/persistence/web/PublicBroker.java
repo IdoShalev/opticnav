449c9adce742e9cbecad4acb7c31f43eb415f921
Kay Bernhardt <kay.bernhardt@edu.sait.ca>
2014-03-24 19:36:11 +0000

    DB: added getUserList method to the public broker
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@200 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- persistence/web-db-broker/src/main/java/opticnav/persistence/web/PublicBroker.java
+++ persistence/web-db-broker/src/main/java/opticnav/persistence/web/PublicBroker.java
@@ -1,16 +1,20 @@
 package opticnav.persistence.web;
 
 import java.sql.CallableStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import opticnav.persistence.web.exceptions.PublicBrokerException;
+import opticnav.persistence.web.map.MapsListEntry;
 
 public class PublicBroker implements AutoCloseable {
     private java.sql.Connection conn;
-    
+
     public PublicBroker(DataSource dataSource) throws SQLException {
         this.conn = DBUtil.getConnectionFromDataSource(dataSource);
     }
@@ -24,40 +28,42 @@ public class PublicBroker implements AutoCloseable {
         }
     }
 
-    public boolean registerAccount(String username, String password) throws PublicBrokerException {
-        if(!checkUsername(username) || !checkPassword(password)){
+    public boolean registerAccount(String username, String password)
+            throws PublicBrokerException {
+        if (!checkUsername(username) || !checkPassword(password)) {
             return false;
-        }
-        else {
-            try (CallableStatement cs = conn.prepareCall("{? = call registerAccount(?, ?)}")) {
+        } else {
+            try (CallableStatement cs = conn
+                    .prepareCall("{? = call registerAccount(?, ?)}")) {
                 cs.setString(2, username);
                 cs.setString(3, password);
                 cs.registerOutParameter(1, Types.BOOLEAN);
-                
+
                 cs.execute();
-                boolean flag = cs.getBoolean(1);                
+                boolean flag = cs.getBoolean(1);
                 conn.commit();
-                
+
                 return flag;
             } catch (SQLException e) {
                 throw new PublicBrokerException(e);
             }
-        }        
-    }    
-    
-    public int verify(String username, String password) throws PublicBrokerException {
-        if(!checkUsername(username) || !checkPassword(password)) {
-            return 0;
         }
-        else {
-            try (CallableStatement cs = conn.prepareCall("{? = call validateUser(?, ?)}")) {
+    }
+
+    public int verify(String username, String password)
+            throws PublicBrokerException {
+        if (!checkUsername(username) || !checkPassword(password)) {
+            return 0;
+        } else {
+            try (CallableStatement cs = conn
+                    .prepareCall("{? = call validateUser(?, ?)}")) {
                 cs.setString(2, username);
                 cs.setString(3, password);
                 cs.registerOutParameter(1, Types.INTEGER);
-                
+
                 cs.execute();
                 int id = cs.getInt(1);
-                return id;            
+                return id;
             } catch (SQLException e) {
                 throw new PublicBrokerException(e);
             }
@@ -65,30 +71,48 @@ public class PublicBroker implements AutoCloseable {
     }
 
     public int findName(String username) throws PublicBrokerException {
-        try (CallableStatement cs = conn.prepareCall("{? = call findAccount(?)}")) {            
+        try (CallableStatement cs = conn
+                .prepareCall("{? = call findAccount(?)}")) {
             cs.registerOutParameter(1, Types.INTEGER);
             cs.setString(2, username);
-            
+
             cs.execute();
             int id = cs.getInt(1);
-            
+
             return id;
         } catch (SQLException e) {
             throw new PublicBrokerException(e);
         }
     }
-    
-    private boolean checkUsername(String username){
+
+    public List<UserARDListEntry> getUserList() throws PublicBrokerException {
+        List<UserARDListEntry> list = new LinkedList<>();
+        try {
+            try (CallableStatement cs = conn.prepareCall("{call getUserList()}")) {
+                try (ResultSet rs = cs.executeQuery()) {
+                    while (rs.next()) {
+                        UserARDListEntry ue = new UserARDListEntry(rs.getString(2), rs.getInt(3));                        
+                        list.add(ue);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new PublicBrokerException(e);
+        }
+        return list;
+    }
+
+    private boolean checkUsername(String username) {
         boolean flag = true;
-        if (username == null || username.equals("")){
+        if (username == null || username.equals("")) {
             flag = false;
         }
         return flag;
     }
-    
-    private boolean checkPassword(String password){
+
+    private boolean checkPassword(String password) {
         boolean flag = true;
-        if (password == null || password.equals("")){
+        if (password == null || password.equals("")) {
             flag = false;
         }
         return flag;
