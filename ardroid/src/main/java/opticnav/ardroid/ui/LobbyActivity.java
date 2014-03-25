32799e6089e7a3aa3be59c97e7152a5aaf30dabb
Dan Spencer <daniel.spencer@edu.sait.ca>
2014-03-25 07:18:33 +0000

    ARDroid: Implemented Lobby list view, it's in a fairly nice state.
    git-svn-id: https://itsd.ict.sait.ca:9880/capstone07/trunk@210 0c6396e7-dfa7-ad41-aac3-ea9b0ac9ed18

--- ardroid/src/main/java/opticnav/ardroid/ui/LobbyActivity.java
+++ ardroid/src/main/java/opticnav/ardroid/ui/LobbyActivity.java
@@ -1,17 +1,59 @@
 package opticnav.ardroid.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import opticnav.ardroid.Application;
+import opticnav.ardroid.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LobbyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+
+        final ListView listView = (ListView)findViewById(R.id.instances_listview);
+
+        final List<String> list = new ArrayList<String>();
+        list.add("Test");
+        list.add("Hello");
+
+        listView.setAdapter(new MySimpleArrayAdapter(this, list));
     }
 
     @Override
     public void onBackPressed() {
         Application.getInstance().getServerUIHandler().tryDisconnect(this);
+    }
+
+    private class MySimpleArrayAdapter extends ArrayAdapter<String> {
+        private final Context context;
+        private final List<String> values;
+
+        public MySimpleArrayAdapter(Context context, List<String> values) {
+            super(context, R.layout.instance_list_item, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.instance_list_item, parent, false);
+            TextView textView = (TextView) rowView.findViewById(R.id.instance_list_item_name);
+            textView.setText(values.get(position));
+
+            return rowView;
+        }
     }
 }
